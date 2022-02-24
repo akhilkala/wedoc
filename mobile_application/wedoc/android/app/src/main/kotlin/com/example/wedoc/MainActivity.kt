e6976bc86dedc58c6af9a9dbeaab7ab8e676e1d7
Akhil Kala <kalaakki@gmail.com>
2022-02-24 11:37:42 +0530

    cleanup

--- mobile_application/wedoc/android/app/src/main/kotlin/com/example/wedoc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.wedoc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobile_application/wedoc/lib/utilities/constants.dart
+++
@@ -1,3 +0,0 @@
-class Constants {
-  static const IS_LOGGED_IN = "IS_LOGGED_IN";
-}
